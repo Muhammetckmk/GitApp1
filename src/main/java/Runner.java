cc1d14f094d933137415646bf8a87beff878ac4c
Muhammet Çakmak <muhammetckmk46@gmail.com>
2024-09-18 13:47:44 +0300

    yeniden v3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,6 +4,5 @@ public class Runner {
         System.out.println("version 1");
         System.out.println("version 2");
         System.out.println("version 3");
-        System.out.println("version 4");
     }
 }
